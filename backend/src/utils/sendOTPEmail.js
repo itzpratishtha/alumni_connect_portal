3dcef7087402be61d46b3e8028ab0918e13a9740
pratishtha <pratishthasomani19@gmail.com>
2026-05-31 22:08:26 +0530

    transporter update

--- backend/src/utils/sendOTPEmail.js
+++ backend/src/utils/sendOTPEmail.js
@@ -8,27 +8,29 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4
+  family: 4,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000
 });
 
 export async function sendOTPEmail(email, otp) {
+
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
+  await transporter.verify();
+  console.log("SMTP READY");
+
   await transporter.sendMail({
     from: `"PIET Alumni Connect" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "Verify Your Email - PIET Alumni Connect",
     html: `
-      <div style="font-family:Arial,sans-serif">
-        <h2>Email Verification</h2>
-        <p>Your OTP is:</p>
-
-        <h1 style="letter-spacing:5px">
-          ${otp}
-        </h1>
-
-        <p>
-          This OTP will expire in 10 minutes.
-        </p>
-      </div>
+      <h2>Email Verification</h2>
+      <h1>${otp}</h1>
     `
   });
+
+  console.log("EMAIL SENT");
 }
