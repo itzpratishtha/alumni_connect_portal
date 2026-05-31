40f401c0f78fce15a1493614be3b0b5953c1f03c
pratishtha <pratishthasomani19@gmail.com>
2026-05-31 23:17:59 +0530

    brevo update

--- backend/src/utils/sendOTPEmail.js
+++ backend/src/utils/sendOTPEmail.js
@@ -1,36 +1,25 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
-  family: 4,
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000
 });
 
 export async function sendOTPEmail(email, otp) {
-
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
-  await transporter.verify();
-  console.log("SMTP READY");
-
   await transporter.sendMail({
-    from: `"PIET Alumni Connect" <${process.env.EMAIL_USER}>`,
+    from: process.env.EMAIL_USER,
     to: email,
     subject: "Verify Your Email - PIET Alumni Connect",
     html: `
       <h2>Email Verification</h2>
+      <p>Your OTP is:</p>
       <h1>${otp}</h1>
+      <p>Expires in 10 minutes.</p>
     `
   });
-
-  console.log("EMAIL SENT");
 }
